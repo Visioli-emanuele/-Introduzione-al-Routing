8a1ed6ad18bf5000f9343d6b880a5728546dedfb
Visioli-emanuele <visioli.emanuele@iisgalvanimi.edu.it>
2022-09-28 08:55:45 +0000

    ultimi ritocchi: inserire const headers

--- spotifay/src/app/spotify.service.ts
+++ spotifay/src/app/spotify.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-
+import { environment } from 'src/environments/environment';
 //Dichiaro che il servizio è iniettabile agli altri componenti a partire dal componente root
 @Injectable({
   providedIn: 'root'  
