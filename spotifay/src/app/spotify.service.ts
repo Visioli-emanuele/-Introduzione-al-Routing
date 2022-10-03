2f11f951be55950511d5973ccc4cbfa5e51c8cfc
Visioli-emanuele <visioli.emanuele@iisgalvanimi.edu.it>
2022-10-03 08:29:42 +0000

    fare punto 3 di 01.2 - [LAB - VER] - Routing Part 3

--- spotifay/src/app/spotify.service.ts
+++ spotifay/src/app/spotify.service.ts
@@ -6,10 +6,7 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root'  
 })
 export class SpotifyService {
-  getArtist(arg0: string): import("rxjs").Observable<Object> {
-    throw new Error('Method not implemented.');
-  }
-   //url per oauth: https://developer.spotify.com/console/get-search-item/
+  //url per oauth: https://developer.spotify.com/console/get-search-item/
   //Ottengo il modulo HttpClient
   constructor(private http: HttpClient) { }
 
@@ -25,7 +22,16 @@ export class SpotifyService {
     const url = `https://api.spotify.com/v1/tracks/${id}`;
     const headers = new HttpHeaders({
       Authorization:
-        'Bearer BQB14oApYHzzvjcjCnDUMp4hH69d5EdKErOTJngCJWNEd-n8iEGX4S7iA--q4OaSk7ChMQndzlAZlvUj6Am-LluCFySxs_cN_mwUjlR58fLfFZeHY5jvJqB-GsNz0o_uEVUdnNi7ADEoKx7INk9ZK4Ew1MTZseruArFeFOOy88UW0J-fYJ_oXNLAD4RGydFKkmM'
+        'Bearer BQAXVPW0BLI27nWWvh2wLMdjpLQ5vT_98Z8jaiZ6SFoZLBP7tUATndO0xRwR6YkIoodFrkog9EyJg_tYcqIatMvN5M28R-UcCcy0nXuS_evxJPH9-uLhm6HxuFpHsKLjdbJ8zYJKR05AiotnA-F-1H-CAyhRXJkkMZVaeTnuUjSYF6SeBMWCfd20G_ojX1E9zaQ'
+    });
+    
+    return this.http.get(url, { headers });
+  }
+  getArtist(id: string) {
+    const url = `https://api.spotify.com/v1/artists/${id}`;
+    const headers = new HttpHeaders({
+      Authorization:
+        'Bearer BQAXVPW0BLI27nWWvh2wLMdjpLQ5vT_98Z8jaiZ6SFoZLBP7tUATndO0xRwR6YkIoodFrkog9EyJg_tYcqIatMvN5M28R-UcCcy0nXuS_evxJPH9-uLhm6HxuFpHsKLjdbJ8zYJKR05AiotnA-F-1H-CAyhRXJkkMZVaeTnuUjSYF6SeBMWCfd20G_ojX1E9zaQ'
     });
     
     return this.http.get(url, { headers });
--- spotifay/src/environments/environment.ts
+++ spotifay/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  oauthToken: "Bearer YOUR_TOKEN"
+  oauthToken: "Bearer BQAXVPW0BLI27nWWvh2wLMdjpLQ5vT_98Z8jaiZ6SFoZLBP7tUATndO0xRwR6YkIoodFrkog9EyJg_tYcqIatMvN5M28R-UcCcy0nXuS_evxJPH9-uLhm6HxuFpHsKLjdbJ8zYJKR05AiotnA-F-1H-CAyhRXJkkMZVaeTnuUjSYF6SeBMWCfd20G_ojX1E9zaQ"
 };
